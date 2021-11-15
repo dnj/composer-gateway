67ea278759b78982033f8ef217c4cb013e2b1b58
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2021-11-15 21:57:03 +0330

    add .json routes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 use dnj\ComposerGateway\Http\Controllers\PackagesController;
 
 Route::get('/packages', [PackagesController::class, 'packages'])->where('path', '.*');
+Route::get('/packages.json', [PackagesController::class, 'packages'])->where('path', '.*');
 Route::get('/{path}/packages', [PackagesController::class, 'packages'])->where('path', '.*');
+Route::get('/{path}/packages.json', [PackagesController::class, 'packages'])->where('path', '.*');
 Route::get('/{path}/p2/{vendor}/{package}', [PackagesController::class, 'packages'])->where('path', '.*');
+Route::get('/{path}/p2/{vendor}/{package}.json', [PackagesController::class, 'packages'])->where('path', '.*');
